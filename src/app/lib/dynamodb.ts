1bdb36b11371f1e6c00ac6e6d2a441508fa97c58
Kingsley Udochukwu <oparaudochukwu277@gmail.com>
2026-06-11 19:12:10 +0100

    fix an error

--- src/app/lib/dynamodb.ts
+++ src/app/lib/dynamodb.ts
@@ -2,10 +2,10 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({
-  region: process.env.AWSS_REGIONS!,
+  region: process.env.REGIONS!,
   credentials: {
-    accessKeyId: process.env.AWSS_ACCESS_KEY_IDS!,
-    secretAccessKey: process.env.AWSS_SECRET_ACCESS_KEYS!,
+    accessKeyId: process.env.ACCESS_KEY_IDS!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEYS!,
   },
 });
 
