0e928c0b191da91d7fa0d71543b2f098922cd64c
Kingsley Udochukwu <oparaudochukwu277@gmail.com>
2026-06-11 19:09:17 +0100

    update the env to fit aws

--- src/app/lib/dynamodb.ts
+++ src/app/lib/dynamodb.ts
@@ -2,10 +2,10 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGIONS!,
+  region: process.env.AWSS_REGIONS!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_IDS!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEYS!,
+    accessKeyId: process.env.AWSS_ACCESS_KEY_IDS!,
+    secretAccessKey: process.env.AWSS_SECRET_ACCESS_KEYS!,
   },
 });
 
